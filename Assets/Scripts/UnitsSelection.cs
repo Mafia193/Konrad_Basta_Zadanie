91a0d72bd316bcf5443e146e701d30091984b57e
Mafia193 <228685@edu.p.lodz.pl>
2021-07-04 13:08:18 +0200

    SelecionBox

--- Assets/Scripts/UnitsSelection.cs
+++ Assets/Scripts/UnitsSelection.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class UnitsSelection : MonoBehaviour {
 
-    private Unit selectedUnit;
+    [SerializeField] RectTransform selectionBox;
+
+    Unit selectedUnit;
+
+    Vector2 startPosition;
+    float width;
+    float height;
+
+    void Awake() {
+        Assert.IsNotNull(selectionBox);
+    }
 
     // Start is called before the first frame update
     void Start() {
-
+        selectionBox.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
+            startPosition = Input.mousePosition;
+            selectionBox.gameObject.SetActive(true);
+
             if (selectedUnit != null) {
                 selectedUnit.Unselect();
                 selectedUnit = null;
             }
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
@@ -26,7 +41,22 @@ public class UnitsSelection : MonoBehaviour {
                     selectedUnit.Select();
                 }
             }
-
         }
+
+        if (Input.GetMouseButton(0)) {
+            drawSelectionBox();
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            selectionBox.gameObject.SetActive(false);
+        }
+    }
+
+    void drawSelectionBox() {
+        width = Input.mousePosition.x - startPosition.x;
+        height = Input.mousePosition.y - startPosition.y;
+
+        selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        selectionBox.anchoredPosition = new Vector2(startPosition.x + width / 2, startPosition.y + height / 2);
     }
 }

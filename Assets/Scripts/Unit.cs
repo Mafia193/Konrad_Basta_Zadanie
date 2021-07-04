bb8a37fa531cee78b5d8f2bab8f75e92c65a5e78
Mafia193 <228685@edu.p.lodz.pl>
2021-07-04 12:27:23 +0200

    Unit

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    [SerializeField] private Color selectedColor;
+    [SerializeField] private Color unselectedColor;
+
+    private Renderer renderer;
+    public bool IsSelected { get; private set; }
+
+    // Start is called before the first frame update
+    void Start() {
+        renderer = GetComponent<Renderer>();
+        Unselect();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Select() {
+        IsSelected = true;
+        renderer.material.color = selectedColor;
+    }
+
+    public void Unselect() {
+        IsSelected = false;
+        renderer.material.color = unselectedColor;
+    }
+}

20603156176c33cee8022cd53d3ccc513a2db220
Mafia193 <228685@edu.p.lodz.pl>
2021-07-05 12:17:49 +0200

    Rings Where the players are going to be placed

--- Assets/Scripts/UnitsController.cs
+++ Assets/Scripts/UnitsController.cs
@@ -6,24 +6,32 @@ using UnityEngine.Assertions;
 public class UnitsController : MonoBehaviour {
 
     [SerializeField] UnitsSelection unitsSelection;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] GameObject Ring;
+
+    List<GameObject> Rings = new List<GameObject>();
     Dictionary<Unit, Vector3> unitsPositions = new Dictionary<Unit, Vector3>();
 
     Vector3 destination;
     Vector3 center = Vector3.zero;
+    Vector3 startPosition;
     int selectionId;
 
     void Awake() {
         Assert.IsNotNull(unitsSelection);
+        Assert.IsNotNull(Ring);
+        Assert.AreNotEqual(0, layerMask);
     }
 
     // Start is called before the first frame update
     void Start() {
-
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetMouseButtonUp(1) && !Input.GetMouseButton(0)) {
+        if (Input.GetMouseButtonDown(1) && !Input.GetMouseButton(0)) {
+            startPosition = Input.mousePosition;
+
             if (selectionId != unitsSelection.SelectionID) {
                 selectionId = unitsSelection.SelectionID;
 
@@ -38,17 +46,52 @@ public class UnitsController : MonoBehaviour {
                     unitsPositions.Add(unit, unit.transform.position - center);
                 }
             }
+            createRings();
+        }
+
+        if (Input.GetMouseButton(1) && !Input.GetMouseButton(0)) {
+            drawPositions();
+        }
+
+        if (Input.GetMouseButtonUp(1) && !Input.GetMouseButton(0)) {
+            destroyRings();
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) {
                 destination = hit.point;
                 foreach (var unit in unitsPositions) {
                     unit.Key.SetDestination(destination + unit.Value);
                 }
-                
+
             }
         }
+    }
 
+    void createRings() {
+        foreach (Unit unit in unitsSelection.selectedUnits) {
+            GameObject ring = Instantiate(Ring) as GameObject;
+            Rings.Add(ring);
+        }
+    }
+
+    void drawPositions() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) {
+            destination = hit.point;
+            int i = unitsPositions.Count;
+            foreach (var unit in unitsPositions) {
+                Vector3 position = destination + unit.Value;
+                Rings[--i].transform.position = new Vector3(position.x, destination.y + 0.01f, position.z);
+            }
+        }
+    }
+
+    void destroyRings() {
+        foreach (GameObject ring in Rings) {
+            Destroy(ring.gameObject);
+        }
+        Rings.Clear();
     }
 }

fbdf7e460759b2006f9e6643d3c4fc8f278d81d3
Mafia193 <228685@edu.p.lodz.pl>
2021-07-06 10:23:17 +0200

    Unit Rotation

--- Assets/Scripts/UnitsController.cs
+++ Assets/Scripts/UnitsController.cs
@@ -9,12 +9,12 @@ public class UnitsController : MonoBehaviour {
     [SerializeField] LayerMask layerMask;
     [SerializeField] GameObject Ring;
 
-    List<GameObject> Rings = new List<GameObject>();
-    Dictionary<Unit, Vector3> unitsPositions = new Dictionary<Unit, Vector3>();
+    List<UnitMovementData> unitMoveDatas = new List<UnitMovementData>();
 
+    Vector3 startMousePostion;
     Vector3 destination;
     Vector3 center = Vector3.zero;
-    Vector3 startPosition;
+
     int selectionId;
 
     void Awake() {
@@ -30,68 +30,99 @@ public class UnitsController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(1) && !Input.GetMouseButton(0)) {
-            startPosition = Input.mousePosition;
-
+            startMousePostion = Input.mousePosition;
             if (selectionId != unitsSelection.SelectionID) {
                 selectionId = unitsSelection.SelectionID;
 
-                center = Vector3.zero;
-                foreach (Unit unit in unitsSelection.selectedUnits) {
-                    center += unit.transform.position;
-                }
-                center /= unitsSelection.selectedUnits.Count;
+                setCenter();
 
-                unitsPositions.Clear();
+                clearUnitsMovesDatas();
                 foreach (Unit unit in unitsSelection.selectedUnits) {
-                    unitsPositions.Add(unit, unit.transform.position - center);
+                    unitMoveDatas.Add(new UnitMovementData(unit, unit.transform.position - center, Instantiate(Ring) as GameObject));
                 }
             }
-            createRings();
-        }
-
-        if (Input.GetMouseButton(1) && !Input.GetMouseButton(0)) {
-            drawPositions();
-        }
-
-        if (Input.GetMouseButtonUp(1) && !Input.GetMouseButton(0)) {
-            destroyRings();
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) {
                 destination = hit.point;
-                foreach (var unit in unitsPositions) {
-                    unit.Key.SetDestination(destination + unit.Value);
-                }
+            }
 
+            activateRings();
+        }
+
+        if (Input.GetMouseButton(1) && !Input.GetMouseButton(0)) {
+            drawPositions();
+        } else {
+            diactivateRings();
+        }
+
+        if (Input.GetMouseButtonUp(1) && !Input.GetMouseButton(0)) {
+            for (int i = unitMoveDatas.Count; i > 0;) {
+                UnitMovementData unitData = unitMoveDatas[--i];
+                unitData.unit.SetDestination(unitData.endPosition);
+                unitData.distanceFromCenter = unitData.endPosition - destination;
             }
         }
     }
 
-    void createRings() {
+    void setCenter() {
+        center = Vector3.zero;
         foreach (Unit unit in unitsSelection.selectedUnits) {
-            GameObject ring = Instantiate(Ring) as GameObject;
-            Rings.Add(ring);
+            center += unit.transform.position;
         }
+        center /= unitsSelection.selectedUnits.Count;
+    }
+    void activateRings() {
+        foreach (UnitMovementData unitData in unitMoveDatas)
+            unitData.ring.SetActive(true);
+    }
+
+    void diactivateRings() {
+        foreach (UnitMovementData unitData in unitMoveDatas)
+            unitData.ring.SetActive(false);
     }
 
     void drawPositions() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) {
-            destination = hit.point;
-            int i = unitsPositions.Count;
-            foreach (var unit in unitsPositions) {
-                Vector3 position = destination + unit.Value;
-                Rings[--i].transform.position = new Vector3(position.x, destination.y + 0.01f, position.z);
-            }
+        Vector3 movement = Input.mousePosition - startMousePostion;
+        float angle = Vector3.Angle(Vector3.up, movement);
+        Vector3 Angle = new Vector3(0, movement.x > 0 ? angle : -angle, 0);
+
+        for (int i = unitMoveDatas.Count; i > 0;) {
+            UnitMovementData unitData = unitMoveDatas[--i];
+
+            unitData.endPosition = destination + unitData.distanceFromCenter;
+            rotatePointAroundPivot(ref unitData.endPosition, destination, Angle);
+            unitData.setRing(destination.y + 0.01f);
         }
     }
 
-    void destroyRings() {
-        foreach (GameObject ring in Rings) {
-            Destroy(ring.gameObject);
+    void rotatePointAroundPivot(ref Vector3 point, Vector3 pivot, Vector3 angles) {
+        point = Quaternion.Euler(angles) * (point - pivot) + pivot;
+    }
+
+    void clearUnitsMovesDatas() {
+        foreach (UnitMovementData unitData in unitMoveDatas)
+            Destroy(unitData.ring.gameObject);
+        unitMoveDatas.Clear();
+    }
+
+    class UnitMovementData {
+        public readonly Unit unit;
+        public Vector3 distanceFromCenter;
+        public Vector3 endPosition;
+        public GameObject ring;
+
+        public UnitMovementData(Unit Unit, Vector3 DistanceFromCenter, GameObject Ring) : this(Unit, DistanceFromCenter, DistanceFromCenter, Ring) { }
+        public UnitMovementData(Unit Unit, Vector3 DistanceFromCenter, Vector3 EndPosition, GameObject Ring) {
+            unit = Unit;
+            distanceFromCenter = DistanceFromCenter;
+            endPosition = EndPosition;
+            ring = Ring;
         }
-        Rings.Clear();
+
+        public void setRing(float hight) {
+            ring.transform.position = new Vector3(endPosition.x, hight, endPosition.z); ;
+        }
     }
 }
